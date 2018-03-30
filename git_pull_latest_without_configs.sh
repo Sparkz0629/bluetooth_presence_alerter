13005ba0aa1bb5450cb5c3f70fe4af71d73256b7
Dexter Rautenbach <Dexter.Rautenbach@standardbank.co.za>
2018-03-30 19:51:19 +0200

    Remove git helper files

--- git_pull_latest_without_configs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#	This is used so that the device list and token file dont get overwritten when doing a git pull
-
-git stash
-git pull
-git stash pop
--- git_push_latest_without_configs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#	This is used so that the device list and token file dont get overwritten when doing a git pull
-
-git stash
-git push
-git stash pop
